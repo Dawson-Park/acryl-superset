91e05f7d9a433631f882432c9174915f0a2d4537
default <default>
2024-03-18 14:38:42 +0900

    calendar 수정

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker stop superset_app superset_worker superset_worker_beat superset_node superset_init superset_nginx
 docker rm superset_app superset_worker superset_worker_beat superset_node superset_init superset_nginx
 superset_id=sudo docker images | grep apachesuperset | awk '{print $3}'
