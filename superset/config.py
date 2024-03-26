50bdcb3dbedc6b1e67958d6358e69961eb6c3515
default <default>
2024-03-26 13:03:01 +0900

    csv encoding 변경 utf-8 -> utf-8-sig

--- superset/config.py
+++ superset/config.py
@@ -793,7 +793,8 @@ CSV_UPLOAD_MAX_SIZE = None
 # CSV Options: key/value pairs that will be passed as argument to DataFrame.to_csv
 # method.
 # note: index option should not be overridden
-CSV_EXPORT = {"encoding": "utf-8"}
+# CSV_EXPORT = {"encoding": "utf-8"}
+CSV_EXPORT = {"encoding": "utf_8_sig"}
 
 # Excel Options: key/value pairs that will be passed as argument to DataFrame.to_excel
 # method.
