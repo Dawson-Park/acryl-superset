e5d6d019a3179b66eb0b43e4a5d148c9dd67e562
Dawson <94957353+Dawson-Park@users.noreply.github.com>
2024-04-20 15:24:37 +0900

    modify shell script

--- start.sh
+++ start.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-docker stop superset_app superset_worker superset_worker_beat superset_node superset_init superset_nginx
-docker rm superset_app superset_worker superset_worker_beat superset_node superset_init superset_nginx
-
-superset_id=$(sudo docker images | grep apachesuperset | awk '{print $3}')
-node_id=$(sudo docker images | grep node | awk '{print $3}')
-echo 'superset_id' $superset_id
-echo 'node_id' $node_id
-
-docker rmi $superset_id $node_id
-
-docker compose up -d
-
-docker ps -a
+docker stop superset_app superset_nginx superset_worker_beat superset_worker superset_node superset_cache superset_db
---
+++ stop.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker start superset_app superset_nginx superset_worker_beat superset_worker superset_node superset_cache superset_db
