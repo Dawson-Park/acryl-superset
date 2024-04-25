bceac6b8a37cee63585a0aef54f102013ef77665
Dawson <94957353+Dawson-Park@users.noreply.github.com>
2024-04-25 16:58:15 +0900

    라우팅 변경

--- superset-frontend/src/pages/Home/index.tsx
+++ superset-frontend/src/pages/Home/index.tsx
@@ -357,10 +357,10 @@ function Welcome({ user, addDangerToast }: WelcomeProps) {
    */
   const histoy = useHistory();
   useEffect(() => {
+    console.log("user", (user as BootstrapUser)?.roles);
     // console.log(Object.keys((user as BootstrapUser)?.roles ?? {}));
     if (Object.keys((user as BootstrapUser)?.roles ?? {}).includes('Client')) {
     // if (!Object.keys((user as BootstrapUser)?.roles ?? {}).includes('Admin')) {
-      console.log("user", (user as BootstrapUser)?.roles);
       document.getElementById('global-navigation-bar')?.remove();
       // @TODO: @dawson URL을 확인해서 파라미터값에 jsessionId가 있으면 dashboard로 이동 아니면 말고
       histoy.push('/superset/dashboard/license_dashboard/');
