ef1024e8421965002360ab8c65836c7501ca2fbf
Dawson <94957353+Dawson-Park@users.noreply.github.com>
2024-04-29 19:54:59 +0900

    nav 설정

--- superset/views/core.py
+++ superset/views/core.py
@@ -943,7 +943,7 @@ class Superset(BaseSupersetView):  # pylint: disable=too-many-public-methods
             if conf["PUBLIC_ROLE_LIKE"]:
 # TODO: @dawson 여기서 url을 읽을 수 있으면 front에서 수정 안해도 구별할 수 있음
 #                 return self.render_template("superset/public_welcome.html")
-                  redirect("/superset/dashboard/license_dashboard?role=guest")
+                  return redirect("/superset/dashboard/license_dashboard")
             return redirect(appbuilder.get_url_for_login)
 
         if welcome_dashboard_id := (
