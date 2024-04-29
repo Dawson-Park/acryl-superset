435fc49247745761ab1c17f1bce11d5533c747c7
Dawson <94957353+Dawson-Park@users.noreply.github.com>
2024-04-29 19:57:41 +0900

    nav 설정

--- superset/views/core.py
+++ superset/views/core.py
@@ -943,7 +943,17 @@ class Superset(BaseSupersetView):  # pylint: disable=too-many-public-methods
             if conf["PUBLIC_ROLE_LIKE"]:
 # TODO: @dawson 여기서 url을 읽을 수 있으면 front에서 수정 안해도 구별할 수 있음
 #                 return self.render_template("superset/public_welcome.html")
-                  return redirect("/superset/dashboard/license_dashboard")
+                  return self.render_template(
+                      "superset/spa.html",
+                      entry="spa",
+                      bootstrap_data=json.dumps(
+                          {
+                              "user": None,
+                              "common": None,
+                          },
+                          default=utils.pessimistic_json_iso_dttm_ser
+                      ),
+                  )
             return redirect(appbuilder.get_url_for_login)
 
         if welcome_dashboard_id := (
